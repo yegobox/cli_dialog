dbe5e302d9eb2fb7927a5ae7562575e4294a8844
Richard Muragijimana <beastar457@gmail.com>
2023-01-19 19:28:33 +0200

    clean-up

--- example/lib/messages.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cli_dialog/cli_dialog.dart';
-
-void main() {
-  final dialog = CLI_Dialog();
-  const messages = ['This is my first message', 'This is my second message'];
-  dialog.addQuestions(messages, is_message: true);
-  dialog.ask();
-  print('lol');
-}
