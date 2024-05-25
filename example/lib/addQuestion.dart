5d8cfc4c7216bff2cec89fad8a06a8154e2d5ecd
Muragijimana <info@yegobox.com>
2024-05-25 21:34:48 +0200

    clean-up

--- example/lib/addQuestion.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cli_dialog/cli_dialog.dart';
-
-void main() {
-  final dialog = CLI_Dialog();
-  dialog.addQuestion('What´up?', 'status');
-  print(dialog.ask()['status']);
-}
--- example/lib/basic_usage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cli_dialog/cli_dialog.dart';
-
-void main() {
-  final dialog = CLI_Dialog(questions: [
-    ['What is your name?', 'name']
-  ]);
-  final name = dialog.ask()['name'];
-  print('Hello $name!');
-}
